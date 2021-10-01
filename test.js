3f7a636fc1bcc7bdaf014644ef3d1dda246ffc8c
masashiabe <tech.abe.jp@gmail.com>
2021-10-02 05:45:09 +0900

    account flow

--- test.js
+++ test.js
@@ -1,7 +1,22 @@
-const stripe = require("stripe")(process.env["SECRET_KEY"]);
+const f = async () => {
+  const stripe = require("stripe")(process.env["SECRET_KEY"]);
 
-const account = await stripe.accounts.create({
-  type: "standard",
-});
+  const account = await stripe.accounts.create({
+    type: "standard",
+  });
 
-console.log(account);
+  console.log(account);
+
+  const accountId = account.id;
+
+  const accountLinks = await stripe.accountLinks.create({
+    account: accountId,
+    refresh_url: "https://example.com/reauth",
+    return_url: "https://example.com/return",
+    type: "account_onboarding",
+  });
+
+  console.log(accountLinks);
+};
+
+f();
